59b975723c0b5a24dcd0d7e7d26132dbeccdcd09
Nabin Prasad Dhungana <prnabin123@gmail.com>
2025-11-26 17:21:29 +0545

    Logic and validation corrected

--- number_guessing/guess_number.py
+++ number_guessing/guess_number.py
@@ -1,16 +1,22 @@
 import random
 
-comp = random.randint(0, 100)
-print("Computer has chosen a number, try to guess it.")
-count = 5
-
 while True:
+    comp = random.randint(0, 100)
+    print("Computer has chosen a number, try to guess it.")
+    count = 5
     while count != 0:
-        user_input = int(input("Try to guess the number between 0 and 100 (including): "))
+        try:
+            user_input = int(input("Try to guess the number between 0 and 100 (including): "))
+        except ValueError:
+            print('Enter valid number only')
+            continue
+
         count -= 1
+        succeed = False
 
         if user_input == comp:
             print("Hurray! You have guessed the correct number.")
+            succeed = True
             break
 
         elif user_input > comp:
@@ -21,12 +27,10 @@ while True:
             print("Too Low!")
             print(f"You have {count} attempts left.")
 
-    if count == 0:
+    if not succeed:
         print("The actual number was", comp)
         print("Better Luck next time!")
-    choice = input("Do you want to play again (y/n): ")
+    choice = input("Do you want to play again (y/n): ").lower()
 
     if choice == 'n':
         break
-    else:
-        count = 5
