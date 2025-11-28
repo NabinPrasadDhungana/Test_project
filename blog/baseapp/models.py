da5dee7672e9d6225be9605f5908f58a1a678c50
Nabin Prasad Dhungana <prnabin123@gmail.com>
2025-11-28 16:15:19 +0545

    added author field to the Blog model, added auth user model settings

--- blog/baseapp/models.py
+++ blog/baseapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings 
 
 # Create your models here.
 class Category(models.Model):
@@ -13,6 +14,7 @@ class Blog(models.Model):
     slug = models.SlugField(unique=True, max_length=200)
     description = models.TextField()
     image = models.ImageField(upload_to='blog_images', blank=True, null=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='blogs')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
