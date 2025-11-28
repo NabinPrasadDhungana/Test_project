662985df87b240c1055570cff891d571c78f2c54
Nabin Prasad Dhungana <prnabin123@gmail.com>
2025-11-28 12:09:39 +0545

    number guessing game finalized

--- number_guessing/guess_number.py
+++ number_guessing/guess_number.py
@@ -3,16 +3,16 @@ import random
 while True:
     comp = random.randint(0, 100)
     print("Computer has chosen a number, try to guess it.")
-    count = 5
-    while count != 0:
+    # count = 5
+    succeed = False
+    while succeed == False:
         try:
             user_input = int(input("Try to guess the number between 0 and 100 (including): "))
         except ValueError:
             print('Enter valid number only')
             continue
 
-        count -= 1
-        succeed = False
+        # count -= 1
 
         if user_input == comp:
             print("Hurray! You have guessed the correct number.")
@@ -21,11 +21,11 @@ while True:
 
         elif user_input > comp:
             print("Too High!")
-            print(f"You have {count} attempts left.")
+            # print(f"You have {count} attempts left.")
         
         else:
             print("Too Low!")
-            print(f"You have {count} attempts left.")
+            # print(f"You have {count} attempts left.")
 
     if not succeed:
         print("The actual number was", comp)
