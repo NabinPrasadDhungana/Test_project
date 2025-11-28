621743e5d83be0261be979cfa725b54e65a883e8
Nabin Prasad Dhungana <prnabin123@gmail.com>
2025-11-28 13:29:52 +0545

    User model, requirements.txt, and .gitignore file created

--- blog/accounts/models.py
+++ blog/accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    name = models.CharField(max_length=150, null=True)
+    avatar = models.ImageField(upload_to='avatars/', default='avatars/default/default.png')
+
+    def __str__(self):
+        return self.username
