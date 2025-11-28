f9b85c97908967c35df2ca8eb55e8c318655ebac
Nabin Prasad Dhungana <prnabin123@gmail.com>
2025-11-28 15:53:47 +0545

    Category and Blog model created

--- blog/baseapp/models.py
+++ blog/baseapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=32, null=True)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Blog(models.Model):
+    title = models.CharField(max_length=150, null=True)
+    slug = models.SlugField(unique=True, max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='blog_images', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
