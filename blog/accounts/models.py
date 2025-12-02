ceb6f8818fb25163b91829ed5cf4dd3fade2866a
Nabin Prasad Dhungana <prnabin123@gmail.com>
2025-12-02 11:13:40 +0545

    default user avatar extension corrected to .jpg

--- blog/accounts/models.py
+++ blog/accounts/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     name = models.CharField(max_length=150, null=True)
-    avatar = models.ImageField(upload_to='avatars/', default='avatars/default/default.png')
+    avatar = models.ImageField(upload_to='avatars/', default='avatars/default/default.jpg')
     email = models.EmailField(unique=True, null=True)
 
     def __str__(self):
